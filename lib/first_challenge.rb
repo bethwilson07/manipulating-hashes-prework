ad289fc50b3284d03994f63cea30fa42c67652c0
Beth Wilson <beth.ny.wlsn@gmail.com>
2018-09-24 19:47:16 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -18,7 +18,7 @@ def first_challenge
       if attribute == :favorite_icecream_flavors
         value.each do |flavor|
           if flavor == "strawberry"
-            flavor.delete
+            flavor.delete("strawberry")
           end
         end
       end
